06d670dd6716de9d4417aa3fd9bc03c65bd9fc40
柴立卫 <15536902091@163.com>
2020-12-24 14:46:44 +0800

    1111

---
+++ src/OperationService/OperationLogService.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace App\Services;
+
+use Record\OperationService\Contracts\OperationLogServiceInterface;
+
+class OperationLogService implements OperationLogServiceInterface
+{
+    public function recordOperation()
+    {
+        //代码
+    }
+}
